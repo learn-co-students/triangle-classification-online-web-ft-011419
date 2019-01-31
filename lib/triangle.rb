5b2bd8f01a13a6766be5b258acc1aa7423045ccb
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-31 00:11:06 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  attr_accessor :a ,:b ,:c
+  def initialize(a,b,c)
+      @a=a
+      @b=b
+      @c=c
+  end
+
+  def kind
+  if   @a == 0 || @a + @b <=@c || @b+@c <=@a ||  @a + @c <=@b
+
+    raise TriangleError
+
+  elsif  @a == @b &&  @b==@c
+      :equilateral
+    elsif
+      @a == @b || @b == @c || @a == @c
+    :isosceles
+    else
+        :scalene
+    end
+  end
+        class TriangleError < StandardError
+
+
+        end
+
+
+
+
+
+
+
 end
