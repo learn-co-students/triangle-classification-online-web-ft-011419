b9b44a9e5980fb640bc0ce38522ba68816038374
Robert McCary <robert.mccary@yahoo.com>
2019-01-31 17:45:53 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+
+  def initialize(first, second, third)
+    @first=first
+    @second=second
+    @third=third
+  end
+
+  def kind
+    if @first < 0 || @second < 0 || @third < 0
+      raise TriangleError
+    elsif @first + @second <= @third || @first + @third <= @second || @second + @third <= @first
+      raise TriangleError
+    else
+      if @first == @second && @second == @third
+        :equilateral
+      elsif  @first == @second || @second == @third || @first == @third
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+
+
+  class TriangleError < StandardError
+
+end
 end
