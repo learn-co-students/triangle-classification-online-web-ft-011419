8233cb7b5916f0618dd3d9236ebdaa91bde5a9cc
Luke Granger <grangerl330@gmail.com>
2019-02-02 01:33:34 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3
+  
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end 
+  
+  def kind 
+    if side_1 == 0 || side_2 == 0 || side_3 == 0 || side_1 + side_2 <= side_3 || side_2 + side_3 <= side_1 || side_1 + side_3 <= side_2
+        raise TriangleError
+    elsif side_1 == side_2 && side_2 == side_3  
+      :equilateral
+    elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
+      :isosceles
+    else 
+      :scalene
+    end 
+  end 
+  
+  class TriangleError < StandardError 
+    "No side can be less than a length of 1 AND two sides added together must always be greater than the third side"
+  end 
+  
 end
